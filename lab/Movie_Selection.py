354b2c3e855c59e7264bb0814b352f5996155add
xinliy <584827526@qq.com>
2017-12-20 11:20:06 +0100

    Update methods to collect top100 movies

--- lab/Movie_Selection.py
+++ lab/Movie_Selection.py
@@ -1,26 +1,18 @@
 
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
 import pandas as pd
 import multiprocessing
-from urllib.error import HTTPError
-
-from functools import reduce
-
 from multiprocessing import Pool
 
-from multiprocessing.dummy import Pool as ThreadPool
 
+# Add local method, otherwise Pool will raise error
 import os
 import sys
 sys.path.append(os.getcwd())
 import collect_method
 
+# Add main function, otherwise Pool will rasie error
 if __name__ == '__main__':
 
-    pool = ThreadPool(5)
-
-    pool1 = multiprocessing.Pool(processes=4)
     movies = ['baywatch_2017', 'the_house_2017', 'snatched_2017',
               'the_artist', 'ratatouille', 'the_disaster_artist',
               'submarine-2010', 'burnt', 'ted_2', 'american_ultra']
@@ -29,32 +21,10 @@ if __name__ == '__main__':
 
         url = 'https://www.rottentomatoes.com/m/' + movie + '/reviews'
 
-        url_list = []
-
-        # Choose the range of the pages
-        for i in range(1, 50):
-
-            page_number = str(i)
-
-            print("Start at page: ", page_number)
-
-            # Adjust the url for every loop
-            url_link = url + "/?page=" + page_number + "&type=user" + "&sort="
-            print('The url address is: ', url_link)
-
-            url_list.append(url_link)
-        print(url_list)
-
-        # Open and get the html content
-
-        # html = urlopen(url_link)
-        # html_list = pool1.map(open_to_read_html, url_list)
-        with Pool(5) as p:
-            pd_list = p.map(collect_method.open_to_read_html, url_list)
-
-        pool.close()
-        pool.join()
-
+        # Add multi threds to speed up.
+        with Pool(15) as p:
+            pd_list = p.map(collect_method.open_to_read_html,
+                            collect_method.create_url_list(url))
         recorder = recorder.append(pd_list)
     recorder.to_csv("TEST1.csv", index=False)
 
--- lab/collect_method.py
+++ lab/collect_method.py
@@ -1,17 +1,78 @@
 
-from urllib.request import urlopen
+2
 from bs4 import BeautifulSoup
 import pandas as pd
-import multiprocessing
 from urllib.error import HTTPError
 
 import re
 
+'''
+  Return a list of top100 movie url
+'''
+def get_top100_url():
 
+    return ['https://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_animation_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_art_house__international_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_classics_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_comedy_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_documentary_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_drama_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_horror_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_kids__family_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_musical__performing_arts_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_mystery__suspense_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_romance_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_science_fiction__fantasy_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_special_interest_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_sports__fitness_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_television_movies/',
+    'https://www.rottentomatoes.com/top/bestofrt/top_100_western_movies/']
+
+
+def get_movie_dict():
+
+    action_movie = ['hacksaw_ridge', 'blood_father', 'deepwater_horizon',
+                    'in_a_valley_of_violence', 'the_magnificent_seven_2016', 'dont_mess_with_texas_2014',
+                    'last_airbender', 'gigli', 'babylon_ad', 'drive_2011', 'fantastic_four_2015', 'mortdecai']
+
+    romance_movie = ['perfect_man', 'everything_everything_2017',
+                     'lost_in_translation', 'enough_said_2013', 'up_in_the_air_2009',
+                     'obvious_child', 'the_duke_of_burgundy', 'la_la_land', 'me_before_you', 'blue_is_the_warmest_color']
+
+    mystery_movie = ['paranoia_2013', 'mobsters',
+                     'the_book_of_henry', 'argo_2012',
+                     'arrival_2016', 'skyfall',
+                     'kingsman_the_secret_service', 'a_most_wanted_man',
+                     'the_drop', 'gone_girl']
+
+    comedy_movie = ['baywatch_2017', 'the_house_2017', 'snatched_2017',
+                    'the_artist', 'ratatouille', 'the_disaster_artist',
+                    'submarine-2010', 'burnt', 'ted_2', 'american_ultra']
+
+    horror_movie = ['the_dark_tower_2017', 'the_mummy_2017', 'wish_upon',
+                    'get_out', 'the_babadook', 'pans_labyrinth', 'the_loved_ones_2012',
+                    'the_conjuring_2', 'the_shallows', 'lights_out_2016', 'ouija_origin_of_evil']
+
+    movie_book = {'action_movie': action_movie, 'romance_movie': romance_movie,
+                  'mystery_movie': mystery_movie, 'comedy_movie': comedy_movie, 'horror_movie': horror_movie}
+    return movie_book
+
+'''
+  Depend on the url extract the movie name.
+  :params str url: Input url
+  :return str : The movie name
+'''
 def extract_name_from_url(url):
     return re.search(r'/m.*/reviews', url).group(0).split("/")[2]
 
+'''
+  Read the html and extract the pos/neg reviews
+  params: str url: the url address
+  params: int MAX_LENGTH(Default:300): the max length for each review
+  return: pd.DataFrame: a df contains all the reviews, with label and movie name
 
+'''
 def open_to_read_html(url, MAX_LENGTH=300):
 
     movie_name = extract_name_from_url(url)
@@ -66,3 +127,49 @@ def open_to_read_html(url, MAX_LENGTH=300):
     except Exception as e:
         print("ERROR")
         raise e
+
+  '''
+  Use url to create all the links of the reviews.
+
+  :param str url: The origin url address 
+  :param int MAX_RANGE: The max range of page for the movie
+  :return list url_list: A list of url in string
+  '''
+def create_url_list(url, MAX_RANGE=50):
+    url_list = []
+    for page in range(1, 50):
+        page_number = str(page)
+        url_link = url + "/?page=" + page_number + "&type=user" + "&sort="
+        url_list.append(url_link)
+    print(url_list)
+    return url_list
+
+
+
+'''
+  Get the name of top 100 movie name
+  :param str url: the origin address
+  :return list movie_list: a list contains all the movie names
+
+'''
+
+def get_top100_movie_list(url):
+    open_url = urlopen(url)
+
+    html = BeautifulSoup(open_url, 'html.parser')
+    movie_list = []
+    for a in html.find('table', class_="table").find_all("a", href=True):
+        href = a['href']
+
+        true_href = re.search(r'm/.*', href)
+        # group() will rasie error if not found
+        try:
+            movie_name = true_href.group(0).split("/")[1]
+            movie_list.append(movie_name)
+
+        except Exception as e:
+            pass
+
+    movie_list = list(set(movie_list))
+    print(movie_list)
+    return movie_list
